53a009af2e192fcd725f2b454d3f2abe01f9c090
aubignat <baptiste.aubignat@etu.unicaen.fr>
2024-09-26 15:16:42 +0200

    première version du readme, quelques ajustements pour le CSS, ajout des icones pour les boutons, ajout des fonctions start() et reset(), timer fonctionnel

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pomodoro
+# Timer Pomodoro
+## Exercice consistant à réaliser un Timer Pomodoro en JavaScript
+:memo Lien du projet :
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,37 @@
-const minutesDepart = 25
-let temps = minutesDepart * 60
+var x = document.getElementById("reset");
+var y = document.getElementById("start");
+x.style.display = "none";
+y.style.display = "block";
 
-const time = document.getElementById("timer")
+const minutesDepart = 25;
+let temps = minutesDepart * 60;
+const min = minutesDepart;
+const defaut = document.getElementById("timer");
 
-setInterval(() => {
-    let secondes = parseInt(temps % 60, 10)
-    let minutes = parseInt(temps / 60, 10)
-    minutes = minutes < 10 ? "0" + minutes: minutes
-    secondes = secondes < 10 ? "0" + secondes: secondes
-    time.innerText = `${minutes}:${secondes}`
-    temps = temps <= 0 ? 0 : temps - 1
-}, 1000)
+defaut.innerText = `${min}:00`;
+
+const time = document.getElementById("timer");
+
+function start() {
+    var x = document.getElementById("reset");
+    var y = document.getElementById("start");
+    x.style.display = "block";
+    y.style.display = "none";
+    setInterval(() => {
+        let secondes = parseInt(temps % 60, 10);
+        let minutes = parseInt(temps / 60, 10);
+        minutes = minutes < 10 ? "0" + minutes: minutes;
+        secondes = secondes < 10 ? "0" + secondes: secondes;
+        time.innerText = `${minutes}:${secondes}`;
+        temps = temps <= 0 ? 0 : temps - 1;
+    }, 1000);
+}
+
+function reset() {
+    /*var x = document.getElementById("reset");
+    var y = document.getElementById("start");
+    x.style.display = "none";
+    y.style.display = "block";
+    document.getElementById("timer").innerText = `${min}:00`;*/
+    location.reload();
+}
