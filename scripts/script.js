2ce4d0911cfa843e875c0c9e0b519d0dd0ec93bc
aubignat <baptiste.aubignat@etu.unicaen.fr>
2024-10-01 18:07:05 +0200

    la pause se déclenche enfin correctement

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 var x = document.getElementById("reset");
 var y = document.getElementById("start");
+
 x.style.display = "none";
 y.style.display = "block";
 
@@ -18,6 +19,7 @@ function start() {
     var y = document.getElementById("start");
     var z = document.getElementById("travail");
     var k = document.getElementById("pause");
+
     x.style.display = "block";
     y.style.display = "none";
     z.style.color = "lime";
@@ -26,16 +28,17 @@ function start() {
     work = setInterval(() => {
         let secondes = parseInt(temps % 60, 10);
         let minutes = parseInt(temps / 60, 10);
+
         minutes = minutes < 10 ? "0" + minutes: minutes;
         secondes = secondes < 10 ? "0" + secondes: secondes;
         time.innerText = `${minutes}:${secondes}`;
         temps = temps <= 0 ? 0 : temps - 1;
-    }, 1000);
 
-    if(secondes == 0 && minutes == 0) {
-        clearInterval(work);
-        pause();
-    }
+        if(temps == 0) {
+            clearInterval(work);
+            pause();
+        }
+    }, 1000);
 }
 
 function reset() {
@@ -44,28 +47,31 @@ function reset() {
     x.style.display = "none";
     y.style.display = "block";
     document.getElementById("timer").innerText = `${min}:00`;*/
+
     location.reload();
 }
 
 function pause() {
     var z = document.getElementById("travail");
     var k = document.getElementById("pause");
+
     z.style.color = "white";
     k.style.color = "lime";
 
     p = setInterval(() => {
         let secondes = parseInt(tempsPause % 60, 10);
         let minutes = parseInt(tempsPause / 60, 10);
+
         minutes = minutes < 10 ? "0" + minutes: minutes;
         secondes = secondes < 10 ? "0" + secondes: secondes;
         time.innerText = `${minutes}:${secondes}`;
         tempsPause = tempsPause <= 0 ? 0 : tempsPause - 1;
-    }, 1000);
 
-    if(secondes == 0 && minutes == 0) {
-        clearInterval(p);
-        temps = minutesDepart * 60;
-        tempsPause = 300;
-        start();
-    }
+        if(tempsPause == 0) {
+            clearInterval(p);
+            temps = minutesDepart * 60;
+            tempsPause = 300;
+            start();
+        }
+    }, 1000);
 }
