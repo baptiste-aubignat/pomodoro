9eadf404b6cb3e68e6aca0e2992ea1a1d2732260
aubignat <baptiste.aubignat@etu.unicaen.fr>
2024-10-01 19:19:43 +0200

    ajout des commentaires HTML et JS

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ defaut.innerText = `${min}:00`;
 
 const time = document.getElementById("timer");
 
+// Fonction start servant à démarrer le minuteur en créant un Intervall, celle-ci est appelée grâce au bouton Start et également à la fin du temps de pause
 function start() {
     var x = document.getElementById("reset");
     var y = document.getElementById("start");
@@ -24,7 +25,8 @@ function start() {
     y.style.display = "none";
     z.style.color = "lime";
     k.style.color = "white";
-
+    
+    // Création de l'Intervall, lorsque celui-ci arrive à 0, il est effacé et la fonction pause() est appelée
     work = setInterval(() => {
         let secondes = parseInt(temps % 60, 10);
         let minutes = parseInt(temps / 60, 10);
@@ -41,6 +43,7 @@ function start() {
     }, 1000);
 }
 
+// Fonction servant à réinitialiser le minuteur, celle-ci est appelée grâce au bouton Reset
 function reset() {
     /*var x = document.getElementById("reset");
     var y = document.getElementById("start");
@@ -51,6 +54,7 @@ function reset() {
     location.reload();
 }
 
+// Fonction servant à lancer un temps de pause, celle-ci est appelée automatiquement une fois le temps de travail terminé et appelle ensuite la fonction start() pour lancer un nouveau minuteur une fois le temps de pause terminé
 function pause() {
     var z = document.getElementById("travail");
     var k = document.getElementById("pause");
@@ -58,6 +62,7 @@ function pause() {
     z.style.color = "white";
     k.style.color = "lime";
 
+    // Création d'un nouvel Intervall pour le temps de pause, une fois qu'il arrive à 0, il est effacé et la fonction start() est appelée
     p = setInterval(() => {
         let secondes = parseInt(tempsPause % 60, 10);
         let minutes = parseInt(tempsPause / 60, 10);
